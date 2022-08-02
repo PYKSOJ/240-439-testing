12fdf3d056eade1b6484a0f818917b4042c5f966
pongsapuk sawaroj <6210110216@email.psu.ac.th>
2022-08-02 22:41:27 +0700

    restructure
    Signed-off-by: pongsapuk sawaroj <6210110216@email.psu.ac.th>

--- project/240-439-testing/coe_number/number_utils.py
+++
@@ -1,9 +0,0 @@
-def is_prime_list(numbers):
-    for num in numbers:
-        for n in range(2, num):
-            if num % n == 0:
-                return False
-            elif num < 0:
-                return False
-    return True
-
---
+++ project/coe_number/number_utils.py
@@ -0,0 +1,9 @@
+def is_prime_list(numbers):
+    for num in numbers:
+        for n in range(2, num):
+            if num % n == 0:
+                return False
+            elif num < 0:
+                return False
+    return True
+
