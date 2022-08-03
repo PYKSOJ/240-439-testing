e0d1c509b69ac210c1b1541637e339d5be2183ed
pyksoj <6210110216@psu.ac.th>
2022-08-03 12:38:30 +0700

    fix defect of 1 0 and negative

--- project/coe_number/number_utils.py
+++ project/coe_number/number_utils.py
@@ -1,9 +1,11 @@
 def is_prime_list(numbers):
     for num in numbers:
-        for n in range(2, num):
-            if num % n == 0:
-                return False
-            elif num < 0:
-                return False
+        if ( num > 1):
+            for n in range(2, num):
+                if num % n == 0:
+                    return False
+        else:
+            return False
     return True
-
+# True == Prime numbers
+# False == Not Prime numbers
--- project/tests/test_number_utils.py
+++ project/tests/test_number_utils.py
@@ -3,28 +3,28 @@ from coe_number.number_utils import is_prime_list
 import unittest
 
 class PrimeListTest(unittest.TestCase): 
-    def test_give_1_2_3_is_prinme(self):    
-        prime_list = [1, 2, 3, 4, 5]
+    def test_give_true_is_prinme(self):    
+        prime_list = [3]
         is_prime = is_prime_list(prime_list)    
         self.assertTrue(is_prime)
-        
+    # Corrct Case
     def test_give_odd_prime_list(self):
         tmp = [2, 4, 6] #2 is prime
         is_prime = is_prime_list(tmp)
         self.assertTrue(is_prime) 
-    
+    # Correct Case
     def test_give_even_prime_list(self):
         tmp = [3, 5, 7] # every argument are prime
         is_prime = is_prime_list(tmp)   
         self.assertTrue(is_prime) 
-    
+    # Correct Case
     def test_give_zero_prime_list(self):
         tmp = [0]
         is_prime = is_prime_list(tmp)
         self.assertTrue(is_prime)
-    
+    # Corrct Case
     def test_give_negative_prime_list(self):
         tmp = [-2]
         is_prime = is_prime_list(tmp)
         self.assertTrue(is_prime)
-        
+    # Corrct Case
