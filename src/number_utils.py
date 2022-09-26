e5eb6933571a67c592a9fc7a1d115279d2f183cb
PYKSOJ <6210110216@psu.ac.th>
2022-09-26 17:40:00 +0700

    fix numbers ultils

--- src/number_utils.py
+++ src/number_utils.py
@@ -1,11 +1,11 @@
 def is_prime_list(numbers):
     for num in numbers:
+        if (num <= 1):
+            return 'error'
         if ( num > 1):
             for n in range(2, num):
                 if num % n == 0:
-                    return False
+                    return "is n\'t Prime numbers"
         else:
             return False
-    return True
-# True == Prime numbers
-# False == Not Prime numbers
+    return 'is Prime numbers'
--- tests/test_number_utils.py
+++ tests/test_number_utils.py
@@ -5,37 +5,44 @@ import unittest
 
 
 class PrimeListTest(unittest.TestCase):
-    def test_give_true_is_prime(self):
+    def test_give_2_return_is_Prime_numbers(self):
         prime_list = [2]
+        expects = 'is Prime numbers'
         is_prime = is_prime_list(prime_list)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_odd_prime_list(self):
+    def test_give_2_4_6_return_is_not_Prime_numbers(self):
         tmp = [2, 4, 6]  # 2 is prime
+        expects = 'is n\'t Prime numbers'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_even_prime_list(self):
+    def test_give_3_5_7_return_is_prime_numbers(self):
         tmp = [3, 5, 7]  # every argument are prime
+        expects = 'is Prime numbers'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_one_prime_list(self):
+    def test_give_1_return_error(self):
         tmp = [1]
+        expects = 'error'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_zero_prime_list(self):
+    def test_give_0_return_error(self):
         tmp = [0]
+        expects = 'error'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_negative_prime_list(self):
+    def test_give_negative_2_return_error(self):
         tmp = [-2]
+        expects = 'error'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
 
-    def test_give_huge_number_prime_list(self):
+    def test_give_27651_return_is_not_Prime_numbers(self):
         tmp = [27651]
+        expects = 'is n\'t Prime numbers'
         is_prime = is_prime_list(tmp)
-        self.assertTrue(is_prime)
+        self.assertEqual(expects, is_prime)
